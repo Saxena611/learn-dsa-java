442ffb48bf1f6f31d9d28b3b2456a91c43555787
animessa <Animesh.Saxena@amdocs.com>
2021-12-28 10:00:26 +0530

    removing out and marking it to gitignore

--- src/linkedlist/SingleLinkedList.java
+++ src/linkedlist/SingleLinkedList.java
@@ -42,8 +42,10 @@ public class SingleLinkedList {
         Node head = new Node(10);
         Node temp1 = new Node(20);
         Node temp2 = new Node(30);
+        Node temp3 = new Node(40);
         head.next = temp1;
         temp1.next = temp2;
+        temp2.next = temp3;
         Node newHead = insertAtBeginning(head,40);
         printLinkedList(newHead);
         Node endHead = insertAtEnd(newHead,50);
