f0ca30578c47aa862470c940b14a1add2f10b857
animessa <Animesh.Saxena@amdocs.com>
2021-12-28 11:06:38 +0530

    adding code for deleting head , tail and adding a node at specific position in a singly linked list

--- src/linkedlist/SingleLinkedList.java
+++ src/linkedlist/SingleLinkedList.java
@@ -28,7 +28,7 @@ public class SingleLinkedList {
         Node last = new Node(x);
         Node curr = head;
         while(curr != null){
-            System.out.println(curr.data);
+            //System.out.println(curr.data);
             if(curr.next == null){
                 break;
             }
@@ -38,18 +38,70 @@ public class SingleLinkedList {
         return head;
     }
 
+    public static Node delHead(Node head){
+        if(head == null){
+            return null;
+        }
+        return head.next;
+    }
+
+    public static Node delTail(Node head){
+        if(head == null){
+            return null;
+        }
+        if(head.next == null){
+            return null;
+        }
+        Node curr = head;
+        while(curr.next.next != null){
+            curr = curr.next;
+        }
+        curr.next = null;
+        return head;
+
+    }
+
+    public static Node insertAtPosition(Node head,int pos,int data){
+        Node temp = new Node(data);
+        if(pos == 1){
+            temp.next = head;
+            return temp;
+        }
+        Node curr = head;
+        for(int i = 1; i <= pos - 2 && curr != null ; i++){
+            curr = curr.next;
+        }
+        if(curr==null){
+            // boundary condition handling
+            // for position exceeding linked list length
+            return head;
+        }
+        temp.next = curr.next; // linking the later part
+        curr.next = temp; // linking the prior part
+        /*
+        * It is necessary to link the later part first
+        * as if you will link the prior part first then you will lose
+        * the track of the latter part of the linked list.
+        * */
+        return head;
+    }
+
     public static void main(String[] args) {
         Node head = new Node(10);
         Node temp1 = new Node(20);
         Node temp2 = new Node(30);
-        Node temp3 = new Node(40);
+        Node temp3 = new Node(50);
         head.next = temp1;
         temp1.next = temp2;
         temp2.next = temp3;
         Node newHead = insertAtBeginning(head,40);
-        printLinkedList(newHead);
-        Node endHead = insertAtEnd(newHead,50);
-        printLinkedList(endHead);
+        //printLinkedList(newHead);
+        Node endHead = insertAtEnd(newHead,60);
+        //printLinkedList(endHead);
+        head = delHead(head);
+        //printLinkedList(head);
+        head = delTail(head);
+        printLinkedList(head);
     }
 
 }
